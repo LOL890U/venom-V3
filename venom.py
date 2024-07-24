99e825b6c89465ce19be9021b1d1e2f836a51b31
LOL890U <jdhdusiidebbarma@gmail.com>
2024-07-24 15:46:28 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7299779165:AAHgZItxKJ_Q90L2DNQ2n1OWLShatndxueQ'
+ADMIN_ID = '5338045956'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
